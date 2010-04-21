f2732e538afd5814ed5084a6667558de9ac99149
SU for Alan Lai <srkirkland@gmail.com>
2010-04-21 16:11:57 +0000

    mapped dispaly profiles. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 5814. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/CRP/Trunk/CRP;C819

---
+++ CRP.Core/Domain/DisplayProfile.cs
@@ -0,0 +1,24 @@
+﻿using UCDArch.Core.NHibernateValidator.Extensions;
+
+namespace CRP.Core.Domain
+{
+    public class DisplayProfile
+    {
+        public DisplayProfile()
+        {
+            
+        }
+
+        public DisplayProfile(string name)
+        {
+            Name = name;
+        }
+
+        [Required]
+        public virtual string Name { get; set; }
+        //TODO: Map to the department class
+        //[Required]
+        //public virtual int DepartmentId { get; set; }
+        public virtual byte[] Logo { get; set; }
+    }
+}
