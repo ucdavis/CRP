2d12f8ae2138b76fe3460207f2088847354ab39c
SU for Alan Lai <srkirkland@gmail.com>
2010-04-21 16:19:17 +0000

    Added tests (Updated by TFS to TFS Connector, server: http://1x:8080/, id 6068. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/CRP/Trunk/CRP;C916

--- CRP.Tests/Controllers/CouponControllerRepositoryTests.cs
+++ CRP.Tests/Controllers/CouponControllerRepositoryTests.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
 using CRP.Controllers;
 using CRP.Controllers.ViewModels;
 using CRP.Core.Domain;
@@ -18,14 +21,19 @@ namespace CRP.Tests.Controllers
     {
         protected List<Item> Items { get; set; }
         protected IRepository<Item> ItemRepository { get; set; }
+        protected readonly IPrincipal Principal = new MockPrincipal();
+        protected IRepository<Coupon> CouponRepository { get; set; }
 
         #region Init
 
         public CouponControllerRepositoryTests()
         {
+            Controller.ControllerContext.HttpContext.User = Principal;
             Items = new List<Item>();
             ItemRepository = FakeRepository<Item>();
             Controller.Repository.Expect(a => a.OfType<Item>()).Return(ItemRepository).Repeat.Any();
+            CouponRepository = FakeRepository<Coupon>();
+            Controller.Repository.Expect(a => a.OfType<Coupon>()).Return(CouponRepository).Repeat.Any();
         }
         /// <summary>
         /// Registers the routes.
@@ -146,6 +154,59 @@ namespace CRP.Tests.Controllers
                 .ToAction<ItemManagementController>(a => a.List());
         }
 
+        /// <summary>
+        /// Tests the create with two parameters and valid data saves.
+        /// </summary>
+        [TestMethod]
+        public void TestCreateWithTwoParametersAndValidDataSaves()
+        {
+            Controller.ControllerContext.HttpContext.Response
+                .Expect(a => a.ApplyAppPathModifier(null)).IgnoreArguments()
+                .Return("http://sample.com/ItemManagement/Edit/2").Repeat.Any();
+            Controller.Url = MockRepository.GenerateStub<UrlHelper>(Controller.ControllerContext.RequestContext);
+
+            FakeItems(3);
+            ItemRepository.Expect(a => a.GetNullableByID(2)).Return(Items[1]).Repeat.Any();
+            var newCoupon = CreateValidEntities.Coupon(1);
+            newCoupon.Code = null;
+            newCoupon.UserId = null;
+
+            var result = Controller.Create(2, newCoupon)
+                .AssertHttpRedirect();
+            Assert.AreEqual("http://sample.com/ItemManagement/Edit/2#Coupons", result.Url);
+            CouponRepository.AssertWasCalled(a => a.EnsurePersistent(newCoupon));
+            Assert.AreEqual("Coupon has been created successfully.", Controller.Message);
+            Assert.IsNotNull(newCoupon.Code);
+            Assert.IsNotNull(newCoupon.UserId);
+        }
+
+        /// <summary>
+        /// Creates the with two parameters and invalid data does not save.
+        /// </summary>
+        [TestMethod]
+        public void CreateWithTwoParametersAndInvalidDataDoesNotSave()
+        {
+            Controller.ControllerContext.HttpContext.Response
+                .Expect(a => a.ApplyAppPathModifier(null)).IgnoreArguments()
+                .Return("http://sample.com/ItemManagement/Edit/2").Repeat.Any();
+            Controller.Url = MockRepository.GenerateStub<UrlHelper>(Controller.ControllerContext.RequestContext);
+
+            FakeItems(3);
+            ItemRepository.Expect(a => a.GetNullableByID(2)).Return(Items[1]).Repeat.Any();
+            var newCoupon = CreateValidEntities.Coupon(1);
+            newCoupon.Code = null;
+            newCoupon.UserId = null;
+            newCoupon.DiscountAmount = 0;
+
+            var result = Controller.Create(2, newCoupon)
+                .AssertViewRendered()
+                .WithViewData<CouponViewModel>();
+            CouponRepository.AssertWasNotCalled(a => a.EnsurePersistent(Arg<Coupon>.Is.Anything));
+            Assert.AreNotEqual("Coupon has been created successfully.", Controller.Message);
+            Assert.AreSame(newCoupon, result.Coupon);
+            Controller.ModelState.AssertErrorsAre("DiscountAmount: must be more than 0.00");
+        }
+
         #endregion Create Tests
 
         #region Helper Methods
@@ -163,6 +224,88 @@ namespace CRP.Tests.Controllers
                 Items[i + offSet].SetIdTo(i + 1 + offSet);
             }
         }
+
+        #region mocks
+        /// <summary>
+        /// Mock the Identity. Used for getting the current user name
+        /// </summary>
+        public class MockIdentity : IIdentity
+        {
+            public string AuthenticationType
+            {
+                get
+                {
+                    return "MockAuthentication";
+                }
+            }
+
+            public bool IsAuthenticated
+            {
+                get
+                {
+                    return true;
+                }
+            }
+
+            public string Name
+            {
+                get
+                {
+                    return "UserName";
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Mock the Principal. Used for getting the current user name
+        /// </summary>
+        public class MockPrincipal : IPrincipal
+        {
+            IIdentity _identity;
+
+            public IIdentity Identity
+            {
+                get
+                {
+                    if (_identity == null)
+                    {
+                        _identity = new MockIdentity();
+                    }
+                    return _identity;
+                }
+            }
+
+            public bool IsInRole(string role)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Mock the HttpContext. Used for getting the current user name
+        /// </summary>
+        public class MockHttpContext : HttpContextBase
+        {
+            private IPrincipal _user;
+
+            public override IPrincipal User
+            {
+                get
+                {
+                    if (_user == null)
+                    {
+                        _user = new MockPrincipal();
+                    }
+                    return _user;
+                }
+                set
+                {
+                    _user = value;
+                }
+            }
+        }
+        #endregion
 
         #endregion Helper Methods
     }
