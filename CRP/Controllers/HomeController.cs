6716761d6483c71df5ba8fb029b9f61a734d4687
SU for Alan Lai <srkirkland@gmail.com>
2010-04-21 16:11:05 +0000

    Added in some NH needed DLLs (like the bytecode provider) and removed the account controller  (Updated by TFS to TFS Connector, server: http://1x:8080/, id 5712. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/CRP/Trunk/CRP;C812

--- CRP/Controllers/HomeController.cs
+++ CRP/Controllers/HomeController.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using UCDArch.Web.Controller;
+using UCDArch.Web.Attributes;
 
 namespace CRP.Controllers
 {
-    [HandleError]
-    public class HomeController : Controller
+    [HandleTransactionsManually]
+    public class HomeController : SuperController
     {
         public ActionResult Index()
         {
