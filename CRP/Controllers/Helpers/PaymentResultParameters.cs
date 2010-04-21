e063153cea8f864559edb66bfee317ce9f6a5c45
SU for Alan Lai <srkirkland@gmail.com>
2010-04-21 16:27:47 +0000

    Added other values returned from touchnet that may be useful (Updated by TFS to TFS Connector, server: http://1x:8080/, id 6559. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/CRP/Trunk/CRP;C1133

--- CRP/Controllers/Helpers/PaymentResultParameters.cs
+++ CRP/Controllers/Helpers/PaymentResultParameters.cs
@@ -8,6 +8,17 @@ namespace CRP.Controllers.Helpers
 {
     public class PaymentResultParameters
     {
+        /*
+            * Since, technically, anyone could post a result to your server to make it think the payment was successful, we recommend following these steps for processing the form post that returns to your server.
+            * Verify the POSTING_KEY against your local copy
+            * Verify UPAY_SITE_ID against expected value(s)
+            * Check that the PMT_STATUS is "success”
+            * Check that the TPG_TRANS_ID is not "DUMMY_TRANS_ID"
+            * Retrieve the transaction from persistent storage using the EXT_TRANS_ID
+            * Verify that the PMT_AMT matches the transaction amount as retrieved from your database
+            * Save any of the returned information that you want to keep (especially TPG_TRANS_ID)
+            * Mark the transaction as "paid” and perform any post-payment processing
+        */ 
         // ReSharper disable InconsistentNaming
         public int? EXT_TRANS_ID { get; set; }
         public string PMT_STATUS { get; set; }
@@ -15,6 +26,17 @@ namespace CRP.Controllers.Helpers
         public decimal? PMT_AMT { get; set; }
         public string TPG_TRANS_ID { get; set; }
         public string CARD_TYPE { get; set; }
+        public string posting_key { get; set; }
+        public string pmt_date { get; set; }
+
+        public string sys_tracking_id { get; set; }
+        public string acct_addr { get; set; }
+        public string acct_addr2 { get; set; }
+        public string acct_city { get; set; }
+        public string acct_state { get; set; }
+        public string acct_zip { get; set; }
+        public string UPAY_SITE_ID { get; set; }
+
         // ReSharper restore InconsistentNaming
     }
 }
