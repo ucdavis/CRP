b2d12b9786403ad62fb22913ccdf75b27d791d7b
SU for Alan Lai <srkirkland@gmail.com>
2010-04-21 16:24:17 +0000

    Added repository tests (Updated by TFS to TFS Connector, server: http://1x:8080/, id 6357. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/CRP/Trunk/CRP;C1034

--- CRP.Tests/Repositories/QuestionTypeRepositoryTests.cs
+++ CRP.Tests/Repositories/QuestionTypeRepositoryTests.cs
@@ -1,8 +1,14 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using CRP.Core.Domain;
 using CRP.Tests.Core;
+using CRP.Tests.Core.Extensions;
 using CRP.Tests.Core.Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Data.NHibernate;
+using UCDArch.Testing.Extensions;
 
 
 namespace CRP.Tests.Repositories
@@ -10,8 +16,13 @@ namespace CRP.Tests.Repositories
     [TestClass]
     public class QuestionTypeRepositoryTests : AbstractRepositoryTests<QuestionType, int >
     {
-        #region Init and Overrides
+        protected IRepository<QuestionType> QuestionTypeRepository { get; set; }
 
+        #region Init and Overrides
+        public QuestionTypeRepositoryTests()
+        {
+            QuestionTypeRepository = new Repository<QuestionType>();
+        }
         /// <summary>
         /// Gets the valid entity of type T
         /// </summary>
@@ -78,6 +89,323 @@ namespace CRP.Tests.Repositories
 
         #endregion Init and Overrides
 
-        //TODO: Other tests
+        #region Name Tests
+        #region Invalid Tests
+
+        /// <summary>
+        /// Tests the name with null value does not save.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void TestNameWithNullValueDoesNotSave()
+        {
+            QuestionType questionType = null;
+            try
+            {
+                #region Arrange
+                questionType = GetValid(9);
+                questionType.Name = null;
+                #endregion Arrange
+
+                #region Act
+                QuestionTypeRepository.DbContext.BeginTransaction();
+                QuestionTypeRepository.EnsurePersistent(questionType);
+                QuestionTypeRepository.DbContext.CommitTransaction();
+                #endregion Act
+            }
+            catch (Exception)
+            {
+                Assert.IsNotNull(questionType);
+                var results = questionType.ValidationResults().AsMessageList();
+                results.AssertErrorsAre("Name: may not be null or empty");
+                Assert.IsTrue(questionType.IsTransient());
+                Assert.IsFalse(questionType.IsValid());
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Tests the name with empty string does not save.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void TestNameWithEmptyStringDoesNotSave()
+        {
+            QuestionType questionType = null;
+            try
+            {
+                #region Arrange
+                questionType = GetValid(9);
+                questionType.Name = string.Empty;
+                #endregion Arrange
+
+                #region Act
+                QuestionTypeRepository.DbContext.BeginTransaction();
+                QuestionTypeRepository.EnsurePersistent(questionType);
+                QuestionTypeRepository.DbContext.CommitTransaction();
+                #endregion Act
+            }
+            catch (Exception)
+            {
+                Assert.IsNotNull(questionType);
+                var results = questionType.ValidationResults().AsMessageList();
+                results.AssertErrorsAre("Name: may not be null or empty");
+                Assert.IsTrue(questionType.IsTransient());
+                Assert.IsFalse(questionType.IsValid());
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Tests the name with spaces only does not save.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void TestNameWithSpacesOnlyDoesNotSave()
+        {
+            QuestionType questionType = null;
+            try
+            {
+                #region Arrange
+                questionType = GetValid(9);
+                questionType.Name = " ";
+                #endregion Arrange
+
+                #region Act
+                QuestionTypeRepository.DbContext.BeginTransaction();
+                QuestionTypeRepository.EnsurePersistent(questionType);
+                QuestionTypeRepository.DbContext.CommitTransaction();
+                #endregion Act
+            }
+            catch (Exception)
+            {
+                Assert.IsNotNull(questionType);
+                var results = questionType.ValidationResults().AsMessageList();
+                results.AssertErrorsAre("Name: may not be null or empty");
+                Assert.IsTrue(questionType.IsTransient());
+                Assert.IsFalse(questionType.IsValid());
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Tests the name with too long value does not save.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void TestNameWithTooLongValueDoesNotSave()
+        {
+            QuestionType questionType = null;
+            try
+            {
+                #region Arrange
+                questionType = GetValid(9);
+                questionType.Name = "x".RepeatTimes(51);
+                #endregion Arrange
+
+                #region Act
+                QuestionTypeRepository.DbContext.BeginTransaction();
+                QuestionTypeRepository.EnsurePersistent(questionType);
+                QuestionTypeRepository.DbContext.CommitTransaction();
+                #endregion Act
+            }
+            catch (Exception)
+            {
+                Assert.IsNotNull(questionType);
+                Assert.AreEqual(51, questionType.Name.Length);
+                var results = questionType.ValidationResults().AsMessageList();
+                results.AssertErrorsAre("Name: length must be between 0 and 50");
+                Assert.IsTrue(questionType.IsTransient());
+                Assert.IsFalse(questionType.IsValid());
+                throw;
+            }
+        }
+        #endregion Invalid Tests
+
+        #region Valid Tests
+
+        /// <summary>
+        /// Tests the name with one character saves.
+        /// </summary>
+        [TestMethod]
+        public void TestNameWithOneCharacterSaves()
+        {
+            #region Arrange
+            var questionType = GetValid(9);
+            questionType.Name = "x";
+            #endregion Arrange
+
+            #region Act
+            QuestionTypeRepository.DbContext.BeginTransaction();
+            QuestionTypeRepository.EnsurePersistent(questionType);
+            QuestionTypeRepository.DbContext.CommitTransaction();
+            #endregion Act
+
+            #region Assert
+            Assert.IsFalse(questionType.IsTransient());
+            Assert.IsTrue(questionType.IsValid());
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the name with long value saves.
+        /// </summary>
+        [TestMethod]
+        public void TestNameWithLongValueSaves()
+        {
+            #region Arrange
+            var questionType = GetValid(9);
+            questionType.Name = "x".RepeatTimes(50);
+            #endregion Arrange
+
+            #region Act
+            QuestionTypeRepository.DbContext.BeginTransaction();
+            QuestionTypeRepository.EnsurePersistent(questionType);
+            QuestionTypeRepository.DbContext.CommitTransaction();
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(50, questionType.Name.Length);
+            Assert.IsFalse(questionType.IsTransient());
+            Assert.IsTrue(questionType.IsValid());
+            #endregion Assert
+        }
+
+        #endregion Valid Tests
+        #endregion Name Tests
+
+        #region HasOptions Tests
+
+        /// <summary>
+        /// Tests the HasOptions is false saves.
+        /// </summary>
+        [TestMethod]
+        public void TestHasOptionsIsFalseSaves()
+        {
+            #region Arrange
+            var questionType = GetValid(9);
+            questionType.HasOptions = false;
+            #endregion Arrange
+
+            #region Act
+            QuestionTypeRepository.DbContext.BeginTransaction();
+            QuestionTypeRepository.EnsurePersistent(questionType);
+            QuestionTypeRepository.DbContext.CommitTransaction();
+            #endregion Act
+
+            #region Assert
+            Assert.IsFalse(questionType.IsTransient());
+            Assert.IsTrue(questionType.IsValid());
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the HasOptions is true saves.
+        /// </summary>
+        [TestMethod]
+        public void TestHasOptionsIsTrueSaves()
+        {
+            #region Arrange
+            var questionType = GetValid(9);
+            questionType.HasOptions = true;
+            #endregion Arrange
+
+            #region Act
+            QuestionTypeRepository.DbContext.BeginTransaction();
+            QuestionTypeRepository.EnsurePersistent(questionType);
+            QuestionTypeRepository.DbContext.CommitTransaction();
+            #endregion Act
+
+            #region Assert
+            Assert.IsFalse(questionType.IsTransient());
+            Assert.IsTrue(questionType.IsValid());
+            #endregion Assert
+        }
+
+        #endregion HasOptions Tests
+
+        #region ExtendedProperty Tests
+
+        /// <summary>
+        /// Tests the ExtendedProperty is false saves.
+        /// </summary>
+        [TestMethod]
+        public void TestExtendedPropertyIsFalseSaves()
+        {
+            #region Arrange
+            var questionType = GetValid(9);
+            questionType.ExtendedProperty = false;
+            #endregion Arrange
+
+            #region Act
+            QuestionTypeRepository.DbContext.BeginTransaction();
+            QuestionTypeRepository.EnsurePersistent(questionType);
+            QuestionTypeRepository.DbContext.CommitTransaction();
+            #endregion Act
+
+            #region Assert
+            Assert.IsFalse(questionType.IsTransient());
+            Assert.IsTrue(questionType.IsValid());
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the ExtendedProperty is true saves.
+        /// </summary>
+        [TestMethod]
+        public void TestExtendedPropertyIsTrueSaves()
+        {
+            #region Arrange
+            var questionType = GetValid(9);
+            questionType.ExtendedProperty = true;
+            #endregion Arrange
+
+            #region Act
+            QuestionTypeRepository.DbContext.BeginTransaction();
+            QuestionTypeRepository.EnsurePersistent(questionType);
+            QuestionTypeRepository.DbContext.CommitTransaction();
+            #endregion Act
+
+            #region Assert
+            Assert.IsFalse(questionType.IsTransient());
+            Assert.IsTrue(questionType.IsValid());
+            #endregion Assert
+        }
+
+        #endregion ExtendedProperty Tests
+
+        #region Reflection of Database
+
+        /// <summary>
+        /// Tests all fields in the database have been tested.
+        /// If this fails and no other tests, it means that a field has been added which has not been tested above.
+        /// </summary>
+        [TestMethod]
+        public void TestAllFieldsInTheDatabaseHaveBeenTested()
+        {
+            #region Arrange
+
+            var expectedFields = new List<NameAndType>();
+            expectedFields.Add(new NameAndType("ExtendedProperty", "System.Boolean", new List<string>()));
+            expectedFields.Add(new NameAndType("HasOptions", "System.Boolean", new List<string>()));
+            expectedFields.Add(new NameAndType("Id", "System.Int32", new List<string>
+            {
+                 "[Newtonsoft.Json.JsonPropertyAttribute()]", 
+                 "[System.Xml.Serialization.XmlIgnoreAttribute()]"
+            }));
+            expectedFields.Add(new NameAndType("Name", "System.String", new List<string>
+            {
+                 "[Newtonsoft.Json.JsonPropertyAttribute()]",
+                 "[NHibernate.Validator.Constraints.LengthAttribute((Int32)50)]", 
+                 "[UCDArch.Core.NHibernateValidator.Extensions.RequiredAttribute()]"
+                 
+            }));
+            #endregion Arrange
+
+            AttributeAndFieldValidation.ValidateFieldsAndAttributes(expectedFields, typeof(QuestionType));
+
+        }
+
+        #endregion Reflection of Database
     }
 }
