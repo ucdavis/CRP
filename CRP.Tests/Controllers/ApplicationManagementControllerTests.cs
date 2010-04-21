d259254bb212a892b9b0745c331cc63f45eb52d8
SU for Alan Lai <srkirkland@gmail.com>
2010-04-21 16:24:55 +0000

    Added Reflection Tests for ApplicationManagementController (Updated by TFS to TFS Connector, server: http://1x:8080/, id 6392. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/CRP/Trunk/CRP;C1051

--- CRP.Tests/Controllers/ApplicationManagementControllerTests.cs
+++ CRP.Tests/Controllers/ApplicationManagementControllerTests.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using CRP.Controllers;
+using CRP.Controllers.Filter;
 using CRP.Controllers.ViewModels;
 using CRP.Core.Domain;
 using CRP.Tests.Core.Extensions;
 using CRP.Tests.Core.Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcContrib.Attributes;
 using MvcContrib.TestHelper;
 using Rhino.Mocks;
 using UCDArch.Core.PersistanceSupport;
 using UCDArch.Testing;
+using UCDArch.Web.Attributes;
 
 namespace CRP.Tests.Controllers
 {
@@ -506,5 +509,271 @@ namespace CRP.Tests.Controllers
         }
 
         #endregion Helper Methods
+
+        #region Reflection Tests
+
+        #region Controller Class Tests
+        /// <summary>
+        /// Tests the controller inherits from super controller.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerInheritsFromSuperController()
+        {
+            #region Arrange
+            var controllerClass = typeof(ApplicationManagementController);
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.BaseType.Name;
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual("SuperController", result);
+            #endregion Assert		
+        }
+
+        /// <summary>
+        /// Tests the controller has only three attributes.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasOnlyThreeAttributes()
+        {
+            #region Arrange
+            var controllerClass = typeof(ApplicationManagementController);
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(3, result.Count());
+            #endregion Assert		
+        }
+        /// <summary>
+        /// Tests the controller has admin only attribute.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasAdminOnlyAttribute()
+        {
+            #region Arrange
+            var controllerClass = typeof(ApplicationManagementController);
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<AdminOnlyAttribute>();
+            #endregion Act
+
+            #region Assert            
+            Assert.IsTrue(result.Count() > 0, "AdminOnlyAttribute not found.");
+            #endregion Assert		
+        }
+
+        /// <summary>
+        /// Tests the controller has transaction attribute.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasTransactionAttribute()
+        {
+            #region Arrange
+            var controllerClass = typeof(ApplicationManagementController);
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<UseTransactionsByDefaultAttribute>();
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(result.Count() > 0, "UseTransactionsByDefaultAttribute not found.");
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller has anti forgery token attribute.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerHasAntiForgeryTokenAttribute()
+        {
+            #region Arrange
+            var controllerClass = typeof(ApplicationManagementController);
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetCustomAttributes(true).OfType<UseAntiForgeryTokenOnPostByDefault>();
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(result.Count() > 0, "UseAntiForgeryTokenOnPostByDefault not found.");
+            #endregion Assert
+        }
+        #endregion Controller Class Tests
+
+        #region Controller Method Tests
+
+        /// <summary>
+        /// Tests the controller contains expected number of public methods.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerContainsExpectedNumberOfPublicMethods()
+        {
+            #region Arrange
+            var controllerClass = typeof(ApplicationManagementController);            
+            #endregion Arrange
+
+            #region Act
+            var result = controllerClass.GetMethods().Where(a => a.DeclaringType == controllerClass);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(7, result.Count(), "It looks like a method was added or removed from the controller.");
+            #endregion Assert		
+        }
+
+
+        /// <summary>
+        /// Tests the controller method list contains expected attributes.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerMethodListContainsExpectedAttributes()
+        {
+            #region Arrange
+            var controllerClass = typeof(ApplicationManagementController);
+            var controllerMethod = controllerClass.GetMethod("Index");
+            #endregion Arrange
+
+            #region Act
+            var result = controllerMethod.GetCustomAttributes(true);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(0, result.Count());
+            #endregion Assert		
+        }
+
+        /// <summary>
+        /// Tests the controller method list item types contains expected attributes.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerMethodListItemTypesContainsExpectedAttributes()
+        {
+            #region Arrange
+            var controllerClass = typeof(ApplicationManagementController);
+            var controllerMethod = controllerClass.GetMethod("ListItemTypes");
+            #endregion Arrange
+
+            #region Act
+            var result = controllerMethod.GetCustomAttributes(true);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(0, result.Count());
+            #endregion Assert
+        }
+        /// <summary>
+        /// Tests the controller method create item type contains expected attributes.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerMethodCreateItemTypeContainsExpectedAttributes()
+        {
+            #region Arrange
+            var controllerClass = typeof(ApplicationManagementController);
+            var controllerMethod = controllerClass.GetMethods().Where(a => a.Name == "CreateItemType");
+            #endregion Arrange
+
+            #region Act
+            var result = controllerMethod.ElementAt(0).GetCustomAttributes(true);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(0, result.Count());
+            #endregion Assert
+        }
+        /// <summary>
+        /// Tests the controller method create item type contains expected attributes2.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerMethodCreateItemTypeContainsExpectedAttributes2()
+        {
+            #region Arrange
+            var controllerClass = typeof(ApplicationManagementController);
+            var controllerMethod = controllerClass.GetMethods().Where(a => a.Name == "CreateItemType");
+            #endregion Arrange
+
+            #region Act
+            var result = controllerMethod.ElementAt(1).GetCustomAttributes(true).OfType<AcceptPostAttribute>();
+
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(1, result.Count());
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller method edit item type contains expected attributes.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerMethodEditItemTypeContainsExpectedAttributes()
+        {
+            #region Arrange
+            var controllerClass = typeof(ApplicationManagementController);
+            var controllerMethod = controllerClass.GetMethods().Where(a => a.Name == "EditItemType");
+            #endregion Arrange
+
+            #region Act
+            var result = controllerMethod.ElementAt(0).GetCustomAttributes(true);
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(0, result.Count());
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller method edit item type contains expected attributes2.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerMethodEditItemTypeContainsExpectedAttributes2()
+        {
+            #region Arrange
+            var controllerClass = typeof(ApplicationManagementController);
+            var controllerMethod = controllerClass.GetMethods().Where(a => a.Name == "EditItemType");
+            #endregion Arrange
+
+            #region Act
+            var result = controllerMethod.ElementAt(1).GetCustomAttributes(true).OfType<AcceptPostAttribute>();
+
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(1, result.Count());
+            #endregion Assert
+        }
+
+        /// <summary>
+        /// Tests the controller method toggle active contains expected attributes.
+        /// </summary>
+        [TestMethod]
+        public void TestControllerMethodToggleActiveContainsExpectedAttributes()
+        {
+            #region Arrange
+            var controllerClass = typeof(ApplicationManagementController);
+            var controllerMethod = controllerClass.GetMethod("ToggleActive");
+            #endregion Arrange
+
+            #region Act
+            var result = controllerMethod.GetCustomAttributes(true).OfType<AcceptPostAttribute>();
+
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(1, result.Count());
+            #endregion Assert
+        }
+
+        #endregion Controller Method Tests
+        #endregion Reflection Tests
+
+
     }
 }
