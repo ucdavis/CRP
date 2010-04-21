aee783720bbf25ab33f45c6dea5bcc2a6a20f388
SU for Alan Lai <srkirkland@gmail.com>
2010-04-21 16:21:02 +0000

    Added ExtendedPropertyAnswer repository tests (Updated by TFS to TFS Connector, server: http://1x:8080/, id 6232. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/CRP/Trunk/CRP;C964

--- CRP.Tests/Repositories/ExtendedPropertyAnswerRepositoryTests.cs
+++ CRP.Tests/Repositories/ExtendedPropertyAnswerRepositoryTests.cs
@@ -1,16 +1,28 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using CRP.Core.Domain;
 using CRP.Tests.Core;
+using CRP.Tests.Core.Extensions;
 using CRP.Tests.Core.Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Data.NHibernate;
+using UCDArch.Testing.Extensions;
 
 namespace CRP.Tests.Repositories
 {
     [TestClass]
     public class ExtendedPropertyAnswerRepositoryTests : AbstractRepositoryTests<ExtendedPropertyAnswer, int>
     {
-        #region Init and Overrides
+        protected IRepository<ExtendedPropertyAnswer> ExtendedPropertyAnswerRepository { get; set; }
 
+        
+        #region Init and Overrides
+        public ExtendedPropertyAnswerRepositoryTests()
+        {
+            ExtendedPropertyAnswerRepository = new Repository<ExtendedPropertyAnswer>();
+        }
         /// <summary>
         /// Gets the valid entity of type T
         /// </summary>
@@ -25,7 +37,7 @@ namespace CRP.Tests.Repositories
         }
 
         /// <summary>
-        /// A Qury which will return a single record
+        /// A Query which will return a single record
         /// </summary>
         /// <param name="numberAtEnd"></param>
         /// <returns></returns>
@@ -99,11 +111,331 @@ namespace CRP.Tests.Repositories
             }
         }
 
-        
-
-
         #endregion Init and Overrides
 
-        //TODO: Other tests
+        #region Answer Tests
+
+        [TestMethod]
+        public void TestExtendedPropertyAnswerWithNullAnswerSaves()
+        {
+            #region Arrange
+            var extendedPropertyAnswer = GetValid(9);
+            extendedPropertyAnswer.Answer = null;
+            #endregion Arrange
+
+            #region Act
+            ExtendedPropertyAnswerRepository.DbContext.BeginTransaction();
+            ExtendedPropertyAnswerRepository.EnsurePersistent(extendedPropertyAnswer);
+            ExtendedPropertyAnswerRepository.DbContext.CommitTransaction();
+            #endregion Act
+
+            #region Assert
+            Assert.IsFalse(extendedPropertyAnswer.IsTransient());
+            Assert.IsTrue(extendedPropertyAnswer.IsValid());
+            #endregion Assert		
+        }
+
+        [TestMethod]
+        public void TestExtendedPropertyAnswerWithEmptyStringAnswerSaves()
+        {
+            #region Arrange
+            var extendedPropertyAnswer = GetValid(9);
+            extendedPropertyAnswer.Answer = string.Empty;
+            #endregion Arrange
+
+            #region Act
+            ExtendedPropertyAnswerRepository.DbContext.BeginTransaction();
+            ExtendedPropertyAnswerRepository.EnsurePersistent(extendedPropertyAnswer);
+            ExtendedPropertyAnswerRepository.DbContext.CommitTransaction();
+            #endregion Act
+
+            #region Assert
+            Assert.IsFalse(extendedPropertyAnswer.IsTransient());
+            Assert.IsTrue(extendedPropertyAnswer.IsValid());
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestExtendedPropertyAnswerWithSpacesOnlyAnswerSaves()
+        {
+            #region Arrange
+            var extendedPropertyAnswer = GetValid(9);
+            extendedPropertyAnswer.Answer = " ";
+            #endregion Arrange
+
+            #region Act
+            ExtendedPropertyAnswerRepository.DbContext.BeginTransaction();
+            ExtendedPropertyAnswerRepository.EnsurePersistent(extendedPropertyAnswer);
+            ExtendedPropertyAnswerRepository.DbContext.CommitTransaction();
+            #endregion Act
+
+            #region Assert
+            Assert.IsFalse(extendedPropertyAnswer.IsTransient());
+            Assert.IsTrue(extendedPropertyAnswer.IsValid());
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestExtendedPropertyAnswerWithOneCharacterAnswerSaves()
+        {
+            #region Arrange
+            var extendedPropertyAnswer = GetValid(9);
+            extendedPropertyAnswer.Answer = "x";
+            #endregion Arrange
+
+            #region Act
+            ExtendedPropertyAnswerRepository.DbContext.BeginTransaction();
+            ExtendedPropertyAnswerRepository.EnsurePersistent(extendedPropertyAnswer);
+            ExtendedPropertyAnswerRepository.DbContext.CommitTransaction();
+            #endregion Act
+
+            #region Assert
+            Assert.IsFalse(extendedPropertyAnswer.IsTransient());
+            Assert.IsTrue(extendedPropertyAnswer.IsValid());
+            #endregion Assert
+        }
+
+        [TestMethod]
+        public void TestExtendedPropertyAnswerWithLongAnswerSaves()
+        {
+            #region Arrange
+            var extendedPropertyAnswer = GetValid(9);
+            extendedPropertyAnswer.Answer = "x".RepeatTimes(250);
+            #endregion Arrange
+
+            #region Act
+            ExtendedPropertyAnswerRepository.DbContext.BeginTransaction();
+            ExtendedPropertyAnswerRepository.EnsurePersistent(extendedPropertyAnswer);
+            ExtendedPropertyAnswerRepository.DbContext.CommitTransaction();
+            #endregion Act
+
+            #region Assert
+            Assert.AreEqual(250, extendedPropertyAnswer.Answer.Length);
+            Assert.IsFalse(extendedPropertyAnswer.IsTransient());
+            Assert.IsTrue(extendedPropertyAnswer.IsValid());
+            #endregion Assert
+        }
+        #endregion Answer Tests
+
+        #region Item Tests
+
+        #region InvalidTests
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void TestExtendedPropertyAnswerWithNullItemDoesNotSave()
+        {
+            ExtendedPropertyAnswer extendedPropertyAnswer = null;
+            try
+            {
+                #region Arrange
+                extendedPropertyAnswer = GetValid(9);
+                extendedPropertyAnswer.Item = null;
+                #endregion Arrange
+
+                #region Act
+                ExtendedPropertyAnswerRepository.DbContext.BeginTransaction();
+                ExtendedPropertyAnswerRepository.EnsurePersistent(extendedPropertyAnswer);
+                ExtendedPropertyAnswerRepository.DbContext.CommitTransaction();
+                #endregion Act
+            }
+            catch (Exception)
+            {
+                #region Assert
+                Assert.IsNotNull(extendedPropertyAnswer);
+                var results = extendedPropertyAnswer.ValidationResults().AsMessageList();
+                results.AssertErrorsAre("Item: may not be empty");
+                Assert.IsTrue(extendedPropertyAnswer.IsTransient());
+                Assert.IsFalse(extendedPropertyAnswer.IsValid());
+                #endregion Assert
+
+                throw;
+            }		
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NHibernate.TransientObjectException))]
+        public void TestExtendedPropertyAnswerWithNewItemDoesNotSave()
+        {
+            ExtendedPropertyAnswer extendedPropertyAnswer = null;
+            try
+            {
+                #region Arrange
+                extendedPropertyAnswer = GetValid(9);
+                extendedPropertyAnswer.Item = new Item();
+                #endregion Arrange
+
+                #region Act
+                ExtendedPropertyAnswerRepository.DbContext.BeginTransaction();
+                ExtendedPropertyAnswerRepository.EnsurePersistent(extendedPropertyAnswer);
+                ExtendedPropertyAnswerRepository.DbContext.CommitTransaction();
+                #endregion Act
+            }
+            catch (Exception ex)
+            {
+                #region Assert
+                Assert.IsNotNull(extendedPropertyAnswer);
+                Assert.IsNotNull(ex);
+                Assert.AreEqual("object references an unsaved transient instance - save the transient instance before flushing. Type: CRP.Core.Domain.Item, Entity: CRP.Core.Domain.Item", ex.Message);
+                #endregion Assert
+
+                throw;
+            }
+        }
+
+        #endregion InvalidTests
+
+        #region Valid Test
+
+        [TestMethod]
+        public void TestExtendedPropertyAnswerWithValidDataSaves()
+        {
+            #region Arrange
+            LoadItems(3);
+            var extendedPropertyAnswer = GetValid(9);
+            extendedPropertyAnswer.Item = Repository.OfType<Item>().GetNullableByID(3);
+            #endregion Arrange
+
+            #region Act
+            ExtendedPropertyAnswerRepository.DbContext.BeginTransaction();
+            ExtendedPropertyAnswerRepository.EnsurePersistent(extendedPropertyAnswer);
+            ExtendedPropertyAnswerRepository.DbContext.CommitTransaction();
+            #endregion Act
+
+            #region Assert
+            Assert.IsFalse(extendedPropertyAnswer.IsTransient());
+            Assert.IsTrue(extendedPropertyAnswer.IsValid());
+            #endregion Assert		
+        }
+        #endregion Valid Test
+        #endregion Item Tests
+
+        #region ExtendedProperty Tests
+
+        #region InvalidTests
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void TestExtendedPropertyAnswerWithNullExtendedPropertyDoesNotSave()
+        {
+            ExtendedPropertyAnswer extendedPropertyAnswer = null;
+            try
+            {
+                #region Arrange
+                extendedPropertyAnswer = GetValid(9);
+                extendedPropertyAnswer.ExtendedProperty = null;
+                #endregion Arrange
+
+                #region Act
+                ExtendedPropertyAnswerRepository.DbContext.BeginTransaction();
+                ExtendedPropertyAnswerRepository.EnsurePersistent(extendedPropertyAnswer);
+                ExtendedPropertyAnswerRepository.DbContext.CommitTransaction();
+                #endregion Act
+            }
+            catch (Exception)
+            {
+                #region Assert
+                Assert.IsNotNull(extendedPropertyAnswer);
+                var results = extendedPropertyAnswer.ValidationResults().AsMessageList();
+                results.AssertErrorsAre("ExtendedProperty: may not be empty");
+                Assert.IsTrue(extendedPropertyAnswer.IsTransient());
+                Assert.IsFalse(extendedPropertyAnswer.IsValid());
+                #endregion Assert
+
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NHibernate.PropertyValueException))]
+        public void TestExtendedPropertyAnswerWithNewExtendedPropertyDoesNotSave()
+        {
+            ExtendedPropertyAnswer extendedPropertyAnswer = null;
+            try
+            {
+                #region Arrange
+                extendedPropertyAnswer = GetValid(9);
+                extendedPropertyAnswer.ExtendedProperty = new ExtendedProperty();
+                #endregion Arrange
+
+                #region Act
+                ExtendedPropertyAnswerRepository.DbContext.BeginTransaction();
+                ExtendedPropertyAnswerRepository.EnsurePersistent(extendedPropertyAnswer);
+                ExtendedPropertyAnswerRepository.DbContext.CommitTransaction();
+                #endregion Act
+            }
+            catch (Exception ex)
+            {
+                #region Assert
+                Assert.IsNotNull(extendedPropertyAnswer);
+                Assert.IsNotNull(ex);
+                Assert.AreEqual("not-null property references a null or transient valueCRP.Core.Domain.ExtendedPropertyAnswer.ExtendedProperty", ex.Message);
+                #endregion Assert
+
+                throw;
+            }
+        }
+
+        #endregion InvalidTests
+
+        #region Valid Test
+
+        [TestMethod]
+        public void TestExtendedPropertyAnswerWithValidExtendedPropertySaves()
+        {
+            #region Arrange
+            LoadExtendedProperty(3);
+            var extendedPropertyAnswer = GetValid(9);
+            extendedPropertyAnswer.ExtendedProperty = Repository.OfType<ExtendedProperty>().GetNullableByID(3);
+            #endregion Arrange
+
+            #region Act
+            ExtendedPropertyAnswerRepository.DbContext.BeginTransaction();
+            ExtendedPropertyAnswerRepository.EnsurePersistent(extendedPropertyAnswer);
+            ExtendedPropertyAnswerRepository.DbContext.CommitTransaction();
+            #endregion Act
+
+            #region Assert
+            Assert.IsFalse(extendedPropertyAnswer.IsTransient());
+            Assert.IsTrue(extendedPropertyAnswer.IsValid());
+            #endregion Assert
+        }
+        #endregion Valid Test
+        #endregion Item Tests
+
+        #region Reflection of Database.
+
+        /// <summary>
+        /// Tests all fields in the database have been tested.
+        /// If this fails and no other tests, it means that a field has been added which has not been tested above.
+        /// </summary>
+        [TestMethod]
+        public void TestAllFieldsInTheDatabaseHaveBeenTested()
+        {
+            #region Arrange
+
+            var expectedFields = new List<NameAndType>();
+            expectedFields.Add(new NameAndType("Answer", "System.String", new List<string>()));
+            expectedFields.Add(new NameAndType("ExtendedProperty", "CRP.Core.Domain.ExtendedProperty", new List<string>{
+                 "[NHibernate.Validator.Constraints.NotNullAttribute()]"
+            }));
+            expectedFields.Add(new NameAndType("Id", "System.Int32", new List<string>
+            {
+                 "[Newtonsoft.Json.JsonPropertyAttribute()]", 
+                 "[System.Xml.Serialization.XmlIgnoreAttribute()]"
+            }));
+            expectedFields.Add(new NameAndType("Item", "CRP.Core.Domain.Item", new List<string>{
+                 "[NHibernate.Validator.Constraints.NotNullAttribute()]"
+            }));
+
+            #endregion Arrange
+
+            AttributeAndFieldValidation.ValidateFieldsAndAttributes(expectedFields, typeof(ExtendedPropertyAnswer));
+
+        }
+
+
+
+        #endregion Reflection of Database.
     }
 }
