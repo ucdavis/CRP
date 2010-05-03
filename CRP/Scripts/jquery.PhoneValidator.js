a1633e43af94c572553be0d232f3d7977e66733e
Sylvestre, Jason <jsylvestre@ucdavis.edu>
2010-05-03 22:11:57 +0000

    added to the regex to allow empty strings for a valid URL or Phone.
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/CRP/Trunk/CRP;C6161

--- CRP/Scripts/jquery.PhoneValidator.js
+++ CRP/Scripts/jquery.PhoneValidator.js
@@ -1,7 +1,7 @@
 ﻿jQuery.validator.addMethod("phoneUS", function(phone_number, element) {
     phone_number = phone_number.replace(/\s+/g, "");
     return this.optional(element) || phone_number.length > 9 &&
-		phone_number.match(/^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/);
+		phone_number.match(/(^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$){1}|^$/);
 }, "Please specify a valid phone number");
 
 
