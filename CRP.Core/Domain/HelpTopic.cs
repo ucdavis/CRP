836c77b03c818e2e97c24650c3a2b41aac292217
Sylvestre, Jason <jsylvestre@ucdavis.edu>
2010-05-27 22:53:15 +0000

    Added helpTopics to domain
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/CRP/Trunk/CRP;C7609

---
+++ CRP.Core/Domain/HelpTopic.cs
@@ -0,0 +1,12 @@
+﻿using UCDArch.Core.DomainModel;
+using UCDArch.Core.NHibernateValidator.Extensions;
+
+namespace CRP.Core.Domain
+{
+    public class HelpTopic : DomainObject
+    {
+        [Required]
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+    }
+}
