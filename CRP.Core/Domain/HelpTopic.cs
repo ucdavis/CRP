44563b5186de9ff92c792dabf36e36c475f24d9d
Sylvestre, Jason <jsylvestre@ucdavis.edu>
2010-05-28 13:33:56 +0000

    Updated database to have a flag to indicate if the general public can view the help topic
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/CRP/Trunk/CRP;C7610

--- CRP.Core/Domain/HelpTopic.cs
+++ CRP.Core/Domain/HelpTopic.cs
@@ -1,4 +1,5 @@
-﻿using UCDArch.Core.DomainModel;
+﻿using NHibernate.Validator.Constraints;
+using UCDArch.Core.DomainModel;
 using UCDArch.Core.NHibernateValidator.Extensions;
 
 namespace CRP.Core.Domain
@@ -8,5 +9,7 @@ namespace CRP.Core.Domain
         [Required]
         public virtual string Name { get; set; }
         public virtual string Description { get; set; }
+        [NotNull]
+        public virtual bool AvailableToPublic { get; set; }
     }
 }
