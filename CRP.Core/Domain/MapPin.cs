33ae3c3587b37f8ad47722a0f980b12b8695a5d7
Sylvestre, Jason <jsylvestre@ucdavis.edu>
2010-10-28 21:32:00 +0000

    Added MapPin
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/CRP/Trunk/CRP;C8548

---
+++ CRP.Core/Domain/MapPin.cs
@@ -0,0 +1,22 @@
+﻿using NHibernate.Validator.Constraints;
+using UCDArch.Core.DomainModel;
+using UCDArch.Core.NHibernateValidator.Extensions;
+
+namespace CRP.Core.Domain
+{
+    public class MapPin : DomainObject
+    {
+        [NotNull]
+        public virtual int ItemId { get; set; }
+
+        public virtual bool IsPrimary { get; set; }
+        [Required]
+        public virtual string Latitude { get; set; }
+        [Required]
+        public virtual string Longitude { get; set; }
+        [Length(50)]
+        public virtual string Title { get; set; }
+        [Length(250)]
+        public virtual string Description { get; set; }
+    }
+}
