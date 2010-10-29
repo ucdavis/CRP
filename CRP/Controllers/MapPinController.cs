2ff57c355f620f61de84e63eff9a331099b394c6
Sylvestre, Jason <jsylvestre@ucdavis.edu>
2010-10-29 18:17:32 +0000

    Added MapPin Controller.
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/CRP/Trunk/CRP;C8556

---
+++ CRP/Controllers/MapPinController.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using CRP.Controllers.Filter;
+using CRP.Core.Domain;
+using MvcContrib.Attributes;
+using UCDArch.Web.Controller;
+
+namespace CRP.Controllers
+{
+    [UserOnly]
+    public class MapPinController : SuperController
+    {
+        //
+        // GET: /MapPin/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /MapPin/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /MapPin/Create
+
+        [AcceptPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            //try
+            //{
+            //    // TODO: Add insert logic here
+
+            //    return RedirectToAction("Index");
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
+            return View();
+        }
+
+        //
+        // GET: /MapPin/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /MapPin/Edit/5
+
+        [AcceptPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [AcceptPost]
+        public ActionResult Remove(int id, int mapPinId)
+        {
+            //TODO :Check that there is editor access
+            var item = Repository.OfType<Item>().GetNullableByID(id);
+            if(item == null)
+            {
+                //TODO: Redirect with message
+            }
+            var mapPin = Repository.OfType<MapPin>().GetNullableByID(mapPinId);
+            if(mapPin == null)
+            {
+                //TODO: Redirect with message
+            }
+            if(!item.MapPins.Contains(mapPin))
+            {
+                //TODO: Redirect with messgae
+            }
+            item.RemoveMapPin(mapPin);
+            //TODO: Return back to map tab with message
+            return View();
+        }
+    }
+}
