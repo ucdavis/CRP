6a14dd765d67f719bc2ead224b9b48be5ac94bd1
Lai, Alan <anlai@ucdavis.edu>
2011-02-28 16:14:50 +0000

    checking in web service stuff so it will compile on team city
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/CRP/Trunk/CRP;C9477

---
+++ CRP/Services/Wcf/IItemService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace CRP.Services.Wcf
+{
+    [ServiceContract]
+    public interface IItemService
+    {
+        [OperationContract]
+        string CreateCoupon(string email, DateTime expiration, int maxQuantity, int maxUsage);
+
+        [OperationContract]
+        bool CancelCoupon(string couponCode);
+
+        [OperationContract]
+        ServiceRegistration GetRegistration(string registrationId);
+
+        [OperationContract]
+        ServiceRegistration[] GetRegistrations(int itemId);
+    }
+
+    public class ServiceRegistration
+    {
+    }
+
+    public class ServiceQuestions
+    {
+    }
+}
---
+++ CRP/Services/Wcf/ItemService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRP.Services.Wcf
+{
+    public class ItemService : IItemService
+    {
+        public string CreateCoupon(string email, DateTime expiration, int maxQuantity, int maxUsage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CancelCoupon(string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceRegistration GetRegistration(string registrationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceRegistration[] GetRegistrations(int itemId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
