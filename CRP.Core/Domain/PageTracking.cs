5fe251af88e80bf5310293aa840570d1502aaed5
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-07-19 15:05:10 -0700

    WIP PageTracking

---
+++ CRP.Core/Domain/PageTracking.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UCDArch.Core.DomainModel;
+
+namespace CRP.Core.Domain
+{
+    public class PageTracking : DomainObject
+    {
+        public PageTracking()
+        {
+            SetDefaults();
+        }
+
+        public PageTracking(string loginId, string location, string ipAddress)
+        {
+            LoginId = loginId;
+            Location = location;
+            IPAddress = ipAddress;
+
+            SetDefaults();
+        }
+
+        private void SetDefaults()
+        {
+            DateTime = DateTime.Now;
+        }
+
+        public virtual string LoginId { get; set; }
+        public virtual string Location { get; set; }
+        public virtual string IPAddress { get; set; }
+        public virtual DateTime DateTime { get; set; }
+    }
+}
---
+++ CRP/Controllers/Helpers/Filter/PageTrackingAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using CRP.Core.Domain;
+using UCDArch.Core;
+using UCDArch.Core.PersistanceSupport;
+
+namespace CRP.Controllers.Helpers.Filter
+{
+    public class PageTrackingAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            try
+            {                
+                var url = filterContext.RequestContext.HttpContext.Request.Url.AbsoluteUri;
+                var login = filterContext.RequestContext.HttpContext.User.Identity.Name;
+                var address = filterContext.RequestContext.HttpContext.Request.UserHostAddress;
+
+                var pageTracking = new PageTracking(login, url, address);
+
+                var repository = SmartServiceLocator<IRepository>.GetService();
+
+                repository.OfType<PageTracking>().EnsurePersistent(pageTracking);
+            }
+            catch
+            {
+                // do nothing, problem with tracking
+            }
+
+            base.OnActionExecuted(filterContext);
+        }
+
+    }
+}
