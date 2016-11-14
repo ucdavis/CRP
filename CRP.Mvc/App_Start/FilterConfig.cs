f9c0f53bcf1482d33f4b7966b6229d9adb393fb8
Jason Sylvestre <jsylvestre@ucdavis.edu>
2016-11-14 13:17:38 -0800

    the log/error filter

--- CRP.Mvc/App_Start/FilterConfig.cs
+++ CRP.Mvc/App_Start/FilterConfig.cs
@@ -1,5 +1,7 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
+using Serilog;
 
 namespace CRP.Mvc
 {
@@ -7,7 +9,19 @@ namespace CRP.Mvc
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            //filters.Add(new HandleErrorAttribute());
+            filters.Add(new HandleAndLogErrorAttribute());
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method,
+         Inherited = true, AllowMultiple = true)]
+    public class HandleAndLogErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            // log exception here via stackify
+            Log.Error(filterContext.Exception, filterContext.Exception.Message);
+            base.OnException(filterContext);
         }
     }
 }
