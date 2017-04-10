67d8721630471c1827cb5d72bfcac0f795bab6a0
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-04-10 15:57:59 -0700

    Not sure if I want this...

--- CRP.Mvc/Controllers/ReportController.cs
+++ CRP.Mvc/Controllers/ReportController.cs
@@ -184,6 +184,98 @@ namespace CRP.Controllers
             return View(viewModel);
         }
 
+        [UserOnly]
+        public ActionResult CreateNew(int itemId)
+        {
+            var item = Repository.OfType<Item>().GetNullableById(itemId);
+
+            if (item == null)
+            {
+                Message = NotificationMessages.STR_ObjectNotFound.Replace(NotificationMessages.ObjectType, "Item");
+                return this.RedirectToAction<ItemManagementController>(a => a.List(null));
+            }
+
+            var viewModel = CreateReportViewModel.Create(Repository, item);
+
+            return View(viewModel);
+        }
+
+        /// <summary>
+        /// POST: /Report/Create/
+        /// </summary>
+        /// <remarks>
+        /// Description:
+        ///     Create a report that is used specifically with one report.
+        /// PreCondition:
+        ///     Item is created, valid and has one or more question sets available
+        ///     createReportParameters parameter has at least one item
+        ///     name is not null or empty
+        /// PostCondition:
+        ///     Item Report is created
+        ///     Each createReportParameters is created into an ItemReportColumn object
+        ///     Item Report is added to an Item
+        /// </remarks>
+        /// <param name="itemId"></param>
+        /// <param name="name"></param>
+        /// <param name="createReportParameters"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [UserOnly]
+        public ActionResult CreateNew(int itemId, string name, CreateReportParameter[] createReportParameters)
+        {
+            var item = Repository.OfType<Item>().GetNullableById(itemId);
+
+            if (item == null)
+            {
+                Message = NotificationMessages.STR_ObjectNotFound.Replace(NotificationMessages.ObjectType, "Item");
+                return this.RedirectToAction<ItemManagementController>(a => a.List(null));
+            }
+
+
+            var report = new ItemReport(name, item,
+                                        Repository.OfType<User>().Queryable.Where(
+                                            a => a.LoginID == CurrentUser.Identity.Name).FirstOrDefault());
+            if (createReportParameters == null || createReportParameters.Count() < 1)
+            {
+                Message = "Report Columns not Selected";
+                var viewModel2 = CreateReportViewModel.Create(Repository, item);
+                viewModel2.ItemReport = report;
+                return View(viewModel2);
+            }
+            foreach (var crp in createReportParameters)
+            {
+                var questionSet = Repository.OfType<QuestionSet>().GetNullableById(crp.QuestionSetId);
+                var question = Repository.OfType<Question>().GetNullableById(crp.QuestionId);
+
+                ItemReportColumn itemReportColumn = crp.Property ? new ItemReportColumn(crp.PropertyName, report)
+                    : new ItemReportColumn(question.Name, report);
+
+                itemReportColumn.Transaction = crp.Transaction;
+                itemReportColumn.Quantity = crp.Quantity;
+                itemReportColumn.Property = crp.Property;
+                itemReportColumn.QuestionSet = questionSet;
+                itemReportColumn.Format = crp.Format;
+
+                report.AddReportColumn(itemReportColumn);
+            }
+
+            MvcValidationAdapter.TransferValidationMessagesTo(ModelState, report.ValidationResults());
+
+            if (ModelState.IsValid)
+            {
+                Repository.OfType<ItemReport>().EnsurePersistent(report);
+                Message = NotificationMessages.STR_ObjectCreated.Replace(NotificationMessages.ObjectType, "Report");
+                //return Redirect(ReturnUrlGenerator.DetailItemUrl(item.Id, StaticValues.Tab_Reports));
+
+                return Redirect(Url.DetailItemUrl(item.Id, StaticValues.Tab_Reports));
+            }
+
+            Message = "Errors with report found.";
+            var viewModel = CreateReportViewModel.Create(Repository, item);
+            viewModel.ItemReport = report;
+            return View(viewModel);
+        }
+
         [AdminOnly]
         public ActionResult ViewSystemReport(int? reportId)
         {
