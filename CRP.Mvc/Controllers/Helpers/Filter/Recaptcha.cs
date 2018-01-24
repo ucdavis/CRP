75117ff53c767024430c0eb923735b1a00e4e7a2
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-01-24 15:21:01 -0800

    Old captcha version one is not used and dies March 31, 2018

--- CRP.Mvc/Controllers/Helpers/Filter/Recaptcha.cs
+++ CRP.Mvc/Controllers/Helpers/Filter/Recaptcha.cs
@@ -10,29 +10,29 @@ namespace CRP.Controllers.Filter
     /// <remarks>
     /// Obtained from http://www.splatto.net/portfolio/blog/post/2009/08/21/Recatcha-In-MVC.aspx
     /// </remarks>
-    public class CaptchaValidatorAttribute : ActionFilterAttribute
-    {
-        private const string CHALLENGE_FIELD_KEY = "recaptcha_challenge_field";
-        private const string RESPONSE_FIELD_KEY = "recaptcha_response_field";
+    //public class CaptchaValidatorAttribute : ActionFilterAttribute
+    //{
+    //    private const string CHALLENGE_FIELD_KEY = "recaptcha_challenge_field";
+    //    private const string RESPONSE_FIELD_KEY = "recaptcha_response_field";
 
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
-            var captchaChallengeValue = filterContext.HttpContext.Request.Form[CHALLENGE_FIELD_KEY];
-            var captchaResponseValue = filterContext.HttpContext.Request.Form[RESPONSE_FIELD_KEY];
-            var captchaValidtor = new Recaptcha.RecaptchaValidator
-            {
-                PrivateKey = CloudConfigurationManager.GetSetting("RecaptchaPrivateKey"),
-                RemoteIP = filterContext.HttpContext.Request.UserHostAddress,
-                Challenge = captchaChallengeValue,
-                Response = captchaResponseValue
-            };
+    //    public override void OnActionExecuting(ActionExecutingContext filterContext)
+    //    {
+    //        var captchaChallengeValue = filterContext.HttpContext.Request.Form[CHALLENGE_FIELD_KEY];
+    //        var captchaResponseValue = filterContext.HttpContext.Request.Form[RESPONSE_FIELD_KEY];
+    //        var captchaValidtor = new Recaptcha.RecaptchaValidator
+    //        {
+    //            PrivateKey = CloudConfigurationManager.GetSetting("RecaptchaPrivateKey"),
+    //            RemoteIP = filterContext.HttpContext.Request.UserHostAddress,
+    //            Challenge = captchaChallengeValue,
+    //            Response = captchaResponseValue
+    //        };
 
-            var recaptchaResponse = captchaValidtor.Validate();
+    //        var recaptchaResponse = captchaValidtor.Validate();
 
-            // this will push the result value into a parameter in our Action
-            filterContext.ActionParameters["captchaValid"] = recaptchaResponse.IsValid;
+    //        // this will push the result value into a parameter in our Action
+    //        filterContext.ActionParameters["captchaValid"] = recaptchaResponse.IsValid;
 
-            base.OnActionExecuting(filterContext);
-        }
-    }
+    //        base.OnActionExecuting(filterContext);
+    //    }
+    //}
 }
