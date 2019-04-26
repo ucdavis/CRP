f23c87610886ef110225678448e0378beaffffd7
John Knoll <jpknoll@gmail.com>
2019-04-26 15:10:40 -0700

    new data signing service

--- CRP.Mvc/ComponentRegistrar.cs
+++ CRP.Mvc/ComponentRegistrar.cs
@@ -7,6 +7,7 @@ using Castle.MicroKernel.Registration;
 using CRP.Controllers.Services;
 using CRP.Core.Abstractions;
 using CRP.Models;
+using CRP.Mvc.Services;
 using CRP.Services;
 
 namespace CRP.Mvc
@@ -20,6 +21,7 @@ namespace CRP.Mvc
             container.Register(Component.For<IValidator>().ImplementedBy<Validator>().Named("validator"));
             container.Register(Component.For<IDbContext>().ImplementedBy<DbContext>().Named("dbContext"));
 
+            container.Register(Component.For<IDataSigningService>().ImplementedBy<DataSigningService>().Named("dataSigningService"));
             container.Register(Component.For<ISearchTermProvider>().ImplementedBy<SearchTermProvider>().Named("searchProvider"));
             container.Register(Component.For<INotificationProvider>().ImplementedBy<NotificationProvider>().Named("NotificationProvider"));
             container.Register(Component.For<IChartProvider>().ImplementedBy<ChartProvider>().Named("ChartProvider"));
---
+++ CRP.Mvc/Services/DataSigningService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Configuration;
+
+namespace CRP.Mvc.Services
+{
+    public interface IDataSigningService
+    {
+        string Sign(IDictionary<string, string> paramsArray);
+        bool Check(IDictionary<string, string> paramsArray, string signature);
+    }
+
+    public class DataSigningService : IDataSigningService
+    {
+        private readonly string _secretKey;
+
+        public DataSigningService()
+        {
+            // yeet. i got you fam. okuuuuur!
+            // so now we can do pair programming remotely
+            // i think you have write permission?
+            // yup i can write anything
+            
+            _secretKey = WebConfigurationManager.AppSettings["CyberSource.SecretKey"];
+        }
+
+        public string Sign(IDictionary<string, string> paramsArray)
+        {
+            return SignData(BuildDataToSign(paramsArray), _secretKey);
+        }
+
+        public bool Check(IDictionary<string, string> paramsArray, string signature)
+        {
+            try
+            {
+                return signature == SignData(BuildDataToSign(paramsArray), _secretKey);
+            }
+            catch (Exception ex)
+            {
+                //Log.Error(ex, ex.Message);
+                return false;
+            }
+        }
+
+        private static string SignData(string data, string secretKey)
+        {
+            var encoding = new UTF8Encoding();
+            byte[] keyByte = encoding.GetBytes(secretKey);
+
+            var hmacsha256 = new HMACSHA256(keyByte);
+            byte[] messageBytes = encoding.GetBytes(data);
+            return Convert.ToBase64String(hmacsha256.ComputeHash(messageBytes));
+        }
+
+        private static string BuildDataToSign(IDictionary<string, string> paramsArray)
+        {
+            var signedFieldNames = paramsArray["signed_field_names"].Split(',');
+            var dataToSign = signedFieldNames.Select(signedFieldName => signedFieldName + "=" + paramsArray[signedFieldName]).ToList();
+
+            return CommaSeparate(dataToSign);
+        }
+
+        private static string CommaSeparate(IEnumerable<string> dataToSign)
+        {
+            return string.Join(",", dataToSign);
+        }
+    }
+}
