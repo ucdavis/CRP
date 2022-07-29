3ce08a74dc900a8fe83ecb51cd4cba9f3473849c
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-07-29 16:42:57 -0700

    more config and better names

--- CRP.Core/Abstractions/IEmailService.cs
+++ CRP.Core/Abstractions/IEmailService.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Mail;
 using Microsoft.Azure;
 
@@ -11,18 +12,21 @@ namespace CRP.Core.Abstractions
 
     public class EmailService : IEmailService
     {
-        private static readonly string UserName = CloudConfigurationManager.GetSetting("CrpEmail");
+        //private static readonly string UserName = CloudConfigurationManager.GetSetting("CrpEmail");
         private static readonly string Password = CloudConfigurationManager.GetSetting("EmailToken");
         private static readonly string Host = CloudConfigurationManager.GetSetting("EmailHost");
         private static readonly string Port = CloudConfigurationManager.GetSetting("EmailPort");
+        private static readonly string UserName = CloudConfigurationManager.GetSetting("EmailUserName");
+        private static readonly string EmailFrom = CloudConfigurationManager.GetSetting("EmailFrom");
 
         public void SendEmail(MailMessage mailMessage)
         {
             mailMessage.Body = $"{mailMessage.Body} <br/><br/><hr> This email is not monitored.<br/>Please do not reply to it.";
             mailMessage.IsBodyHtml = true;
-            
 
-            mailMessage.From = new MailAddress("registration-notify@ucdavis.edu"); 
+
+            //mailMessage.From = new MailAddress("registration-notify@ucdavis.edu");
+            mailMessage.From = new MailAddress(EmailFrom);
             if (mailMessage.IsBodyHtml)
             {
                 mailMessage.IsBodyHtml = false;
@@ -39,9 +43,7 @@ namespace CRP.Core.Abstractions
                 client.Port = int.Parse(Port);
                 client.DeliveryMethod = SmtpDeliveryMethod.Network;
                 client.EnableSsl = true;
-
                 client.Send(mailMessage);
-
             }
         }
     }
