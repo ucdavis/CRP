eea95ec6b78b6a2d1c9496b1800cbfa1347f025f
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-08-01 15:15:41 -0700

    Copy Paste TSL

--- CRP.Mvc/Global.asax.cs
+++ CRP.Mvc/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -12,6 +13,7 @@ namespace CRP.Mvc
     {
         protected void Application_Start()
         {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             LogConfig.ConfigureLogging();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
