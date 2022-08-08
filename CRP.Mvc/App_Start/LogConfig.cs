cf8f7b58b2e32a5900601da7d24c77bab1489229
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-08-08 09:32:42 -0700

    ES Logging

--- CRP.Mvc/App_Start/LogConfig.cs
+++ CRP.Mvc/App_Start/LogConfig.cs
@@ -1,7 +1,10 @@
-﻿using System.Web;
+﻿using System;
+using System.Configuration;
+using System.Web;
 using FluentNHibernate.Utils;
 using Serilog;
 using Serilog.Exceptions.Destructurers;
+using Serilog.Sinks.Elasticsearch;
 using SerilogWeb.Classic.Enrichers;
 
 namespace CRP.Mvc
@@ -18,15 +21,37 @@ namespace CRP.Mvc
             if (_loggingSetup) return; //only setup logging once
 
             Log.Logger = new LoggerConfiguration()
-                .WriteTo.Stackify()
+                .WriteToElasticSearchCustom()
+                //.WriteTo.Stackify()
                 .Filter.ByExcluding(a => a.Exception != null && a.Exception.GetBaseException() is HttpException)
                 .Enrich.With<HttpSessionIdEnricher>()
                 .Enrich.With<UserNameEnricher>()
                 .Enrich.With<ExceptionEnricher>()
                 .Enrich.FromLogContext()
+                .Enrich.WithClientIp()
+                .Enrich.WithClientAgent()
                 .CreateLogger();
 
             _loggingSetup = true;
+        }
+
+        public static LoggerConfiguration WriteToElasticSearchCustom(this LoggerConfiguration logConfig)
+        {
+            var esUrl = ConfigurationManager.AppSettings["Stackify.ElasticUrl"];
+
+            // only continue if a valid http url is setup in the config
+            if (esUrl == null || !esUrl.StartsWith("http"))
+            {
+                return logConfig;
+            }
+
+            logConfig.Enrich.WithProperty("Application", ConfigurationManager.AppSettings["Stackify.AppName"]);
+            logConfig.Enrich.WithProperty("AppEnvironment", ConfigurationManager.AppSettings["Stackify.Environment"]);
+
+            return logConfig.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(esUrl))
+            {
+                IndexFormat = "aspnet-registration-{0:yyyy.MM}"
+            });
         }
     }
 }
